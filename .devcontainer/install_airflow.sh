1b2803f8ad08d10d9262c1752c6f6f5852bad202
RamiKrispin <ramkrisp@umich.edu>
2024-06-03 10:21:32 -0700

    add build arguments

--- .devcontainer/install_airflow.sh
+++ .devcontainer/install_airflow.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 VENV_NAME=$1
+USERNAME=$2
+FIRST=$3
+LAST=$4 
+ROLE=$5 
+PASSWORD=$6 
+EMAIL=$7
+
 
 source /opt/$VENV_NAME/bin/activat
 
@@ -18,15 +25,12 @@ pip install apache-airflow-providers-docker
 
 airflow db migrate 
 airflow users create \
-    --username admin \
-    --firstname Rami \
-    --lastname Krispin \
-    --role Admin \
-    --password pass \
-    --email my_email@domain.com 
-
-
-
+    --username $USERNAME \
+    --firstname $FIRST \
+    --lastname $LAST \
+    --role $ROLE \
+    --password $PASSWORD \
+    --email $EMAIL
 
 # airflow webserver --port 8080
 # airflow scheduler
